44b44f46724b4c07b4137268c666d4b79137a028
tatyyukie <tatyy-yukie@hotmail.com>
2022-08-16 17:59:30 -0300

    Adicionando funcionalidade para selecionar elemento na página de seguros

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log("Teste");
+//Ativar links do menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
+    if (url.includes(href)){
+       link.classList.add("ativo");
+    }
+}
+
+links.forEach(ativarLink);
+
+//Ativar items do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(paramentro){
+    const elemento = document.getElementById(paramentro);
+    
+    if (elemtento){
+        elemento.checked = true;
+    }
+
+    elemento.checked = true;
+    console.log(elemento);
+}
+
+parametros.forEach(ativarProduto);
